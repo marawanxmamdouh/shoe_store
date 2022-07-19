d05a4489e1e65c6a1db9b9fe4ba281ca28f56c06
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-19 14:29:37 +0200

    Setup ActionBar back button

--- app/src/main/java/dev/marawanxmamdouh/shoestore/ui/MainActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/shoestore/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package dev.marawanxmamdouh.shoestore.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import dev.marawanxmamdouh.shoestore.R
 import dev.marawanxmamdouh.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Find NavHostFragment and NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navigationController = navHostFragment.navController
+
+        // Setup ActionBar back button
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
+
         setContentView(binding.root)
+    }
+
+    // Make back button work
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
