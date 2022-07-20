f0d9adc3bd7e8d074e8226ea20cff4ad6ab66bac
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-20 02:34:28 +0200

    Add menu action

--- app/src/main/java/dev/marawanxmamdouh/shoestore/ui/shoelist/ShoeList.kt
+++ app/src/main/java/dev/marawanxmamdouh/shoestore/ui/shoelist/ShoeList.kt
@@ -1,10 +1,13 @@
 package dev.marawanxmamdouh.shoestore.ui.shoelist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import dev.marawanxmamdouh.shoestore.R
 import dev.marawanxmamdouh.shoestore.databinding.FragmentShoeListBinding
 
 class ShoeList : Fragment() {
@@ -18,5 +21,26 @@ class ShoeList : Fragment() {
         binding = FragmentShoeListBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.overflow_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.miLogout -> {
+                        view.findNavController().navigate(R.id.loginFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
