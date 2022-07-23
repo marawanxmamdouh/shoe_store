e5e4664ab282604f155ff4b66b43ec7c28e32c5c
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-23 11:37:32 +0200

    Create shared view model
    With MutableLiveData property (shoeList) setter and getter

---
+++ app/src/main/java/dev/marawanxmamdouh/shoestore/ui/SharedViewModel.kt
@@ -0,0 +1,22 @@
+package dev.marawanxmamdouh.shoestore.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dev.marawanxmamdouh.shoestore.models.Shoe
+
+class SharedViewModel: ViewModel() {
+
+    private var _shoeList = MutableLiveData<MutableList<Shoe>>()
+    val shoeList: LiveData<MutableList<Shoe>>
+        get() = _shoeList
+
+    init {
+        _shoeList.value = ArrayList()
+    }
+
+    fun addShoe(shoe: Shoe){
+        _shoeList.value?.add(shoe)
+        _shoeList.value = _shoeList.value
+    }
+}
